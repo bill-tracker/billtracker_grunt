46b446387022e1890a799d5553ddffafcc516f7d
Mark McDermott <mcdermottsolutions@gmail.com>
2016-06-04 09:24:01 -0500

    fixed blog page

--- dev/billtracker/bills/urls.py
+++ dev/billtracker/bills/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'bills'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about-us/', views.about_us, name='about_us'), 
-    url(r'^bill-listing/', views.about_us, name='bill_listing'),
-    url(r'^blog/', views.about_us, name='blog'),
-    url(r'^contact-us/', views.about_us, name='contact_us'),
+    url(r'^about-us/', views.about_us, name='about_us'),
+    url(r'^bill-listing/', views.bill_listing, name='bill_listing'),
+    url(r'^blog/', views.blog, name='blog'),
+    url(r'^contact-us/', views.contact_us, name='contact_us'),
     url(r'^bills/(?P<bill_id>[0-9]+)/$', views.single, name='single'),
     url(r'^search_by_title/', views.search_by_title, name='search_by_title'),
     url(r'^scraper/', views.scraper, name='scraper'),
