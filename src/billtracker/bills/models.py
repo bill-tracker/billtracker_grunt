9399139582546b8b2ea300df8c7f8458ab33ee3f
John Crowe <thecrow1213@gmail.com>
2016-06-04 18:18:17 -0500

    WIP #4

--- src/billtracker/bills/models.py
+++ src/billtracker/bills/models.py
@@ -19,3 +19,36 @@ class Bill(models.Model):
 
     def __str__(self):
         return self.title
+
+class BillRevision(models.Model):
+    doc_id = models.IntegerField()
+    bill_id = models.IntegerField()
+    date = models.DateField()
+    doc_type = models.CharField(max_length=50, default="")
+    doc_mime = models.CharField(max_length=50, default="")
+    doc_encoded = models.TextField(default="")
+
+class Session(models.Model):
+    name = models.CharField(max_length=500, default="")
+    session_name = models.CharField(max_length=500, default="")
+    special = models.BooleanField()
+    year_start = models.IntegerField()
+    year_end = models.IntegerField()
+    state_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Sponsor(models.Model):
+    party_id = models.CharField(max_length=1, default="")
+    party = models.CharField(max_length=1, default="")
+    role = models.CharField(max_length=10, default="")
+    name = models.CharField(max_length=10, default="")
+    first_name = models.CharField(max_length=10, default="")
+    middle_name = models.CharField(max_length=10, default="")
+    last_name = models.CharField(max_length=10, default="")
+    district = models.CharField(max_length=10, default="")
+    follow_the_money_id = models.CharField(max_length=10, default="")
+
+    def __str__(self):
+        return self.name
--- src/billtracker/bills/services/legiscanService.py
+++ src/billtracker/bills/services/legiscanService.py
@@ -6,9 +6,57 @@ class LegiscanService():
     base_params = { 'key': settings.LEGISCAN_API_KEY }
 
     @staticmethod
-    def getMasterList(state):
-        params = { 'op': 'getMasterList', 'state': state }
+    def getMasterList(state=None, session=None):
+        params = { 'op': 'getMasterList' }
         params.update(LegiscanService.base_params)
+        if state is not None: params.update({ 'state': state })
+        if session is not None: params.update({ 'id': session })
         response = requests.get(LegiscanService.base_url, params)
         response.raise_for_status()
         return response.json()
+
+    @staticmethod
+    def getBill(bill_id, state):
+        params = {'op': 'getBill', 'bill_id':bill_id,'state':state}
+        params.update(LegiscanService.base_params)
+        response = requests.get(LegiscanService.base_url, params)
+        response.raise_for_status()
+        return repsonse.json()
+
+    @staticmethod
+    def getBillText(doc_id):
+         params = {'op': 'getBillText','doc_id':doc_id}
+         params.update(LegiscanService.base_params)
+         response = requests.get(LegiscanService.base_url, params)
+         response.raise_for_status()
+         return response.json()
+
+    @staticmethod
+    def getAmedment(amendment_id):
+         params = {'op': 'getAmendment', 'amendment_id':amendment_id}
+         params.update(LegiscanService.base_params)
+         response = requests.get(LegiscanService.base_url, params)
+         response.raise_for_status()
+         return response.json()
+    @staticmethod
+    def getSupplement(supplement_id):
+         params = {'op': 'getSupplement', 'supplement_id':supplement_id}
+         params.update(LegiscanService.base_params)
+         response = requests.get(LegiscanService.base_url, params)
+         response.raise_for_status()
+         return response.json()
+    @staticmethod
+    def getRoll(roll_call_id):
+         params = {'op': 'getSupplement', 'supplement_id':supplement_id}
+         params.update(LegiscanService.base_params)
+         response = requests.get(LegiscanService.base_urls, params)
+         response.raise_for_status()
+         return response.json()
+
+    @staticmethod
+    def getSponser(people_id):
+         params = {'op': 'getSponser', 'people_id':people_id}
+         params.update(LegiscanService.base_params)
+         response = response.get(LegiscanService.base_urls, params)
+         response.raise_for_status()
+         return response.json()
