b5e426aa27c984f338f1b350ac9fb178cb55aebb
John Crowe <thecrow1213@gmail.com>
2016-06-04 16:19:09 -0500

    WIP #4, got bills saving to sqlite

--- src/billtracker/bills/management/commands/crawlLegiscan.py
+++ src/billtracker/bills/management/commands/crawlLegiscan.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.dateparse import parse_date
 from bills.models import *
 from bills.services.legiscanService import LegiscanService
 import requests
@@ -7,5 +8,17 @@ class Command(BaseCommand):
     help = 'Pulls sponsor and bill data from LegiScan'
 
     def handle(self, *args, **options):
+        print 'Resetting bills...'
+        Bill.objects.all().delete()
         print 'Pulling master list...'
-        print LegiscanService.getMasterList('TX')
+        masterList = LegiscanService.getMasterList('TX')
+
+        for key, bill in masterList['masterlist'].iteritems():
+            if 'bill_id' in bill:
+                print 'Processing bill: ' + str(bill['bill_id'])
+                newBill = Bill(id=bill['bill_id'], number=bill['number'], title=bill['title'],
+                            description=bill['description'], url=bill['url'],
+                            last_action=bill['last_action'], last_action_date=parse_date(bill['last_action_date']),
+                            status=bill['status'],
+                            status_date=parse_date(bill['status_date']) if bill['status_date'] is not None else None)
+                newBill.save()
--- src/billtracker/bills/models.py
+++ src/billtracker/bills/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 class Bill(models.Model):
+    number = models.CharField(max_length=500, default="")
     title = models.CharField(max_length=500, default="")
-    text = models.CharField(max_length=10000, default="")
+    description = models.TextField(default="")
+
+    last_action_date = models.DateField(null=True)
+    last_action = models.CharField(max_length=500, null=True)
+
+    status_date = models.DateField(null=True)
+    status = models.CharField(max_length=500, null=True)
+
+    url = models.CharField(max_length=500, null=True)
+
     date = models.DateField(auto_now_add = True, editable=False)
     get_latest_by = 'date'
+
     def __str__(self):
         return self.title
