f670802724bbbd624285314566e46db95ba9318f
John Crowe <thecrow1213@gmail.com>
2016-06-11 13:33:26 -0500

    Putting data in detail pages.

--- src/billtracker/bills/urls.py
+++ src/billtracker/bills/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^blog/', views.blog, name='blog'),
     url(r'^contact-us/', views.contact_us, name='contact_us'),
     url(r'^bills/(?P<bill_id>[0-9]+)/$', views.single, name='single'),
+    url(r'^bills/(?P<bill_id>[0-9]+)/revision/(?P<rev_id>[0-9]+)/$', views.revision, name='revision'),
     url(r'^search_by_title/', views.search_by_title, name='search_by_title'),
     url(r'^scraper/', views.scraper, name='scraper'),
 ]
--- src/billtracker/bills/views.py
+++ src/billtracker/bills/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Bill
+from .models import *
 import requests
+import base64
+import re
 from bs4 import BeautifulSoup
 
 def index(request):
-    bills = Bill.objects.order_by('-date')
-    return render(request, 'bills/index.html', {'bills':bills})
+    return render(request, 'bills/index.html')
 
 def about_us(request):
     return render(request, 'bills/about.html')
 
 def bill_listing(request):
-    bills = Bill.objects.order_by('-date')
-    return render(request, 'bills/bill-listing.html', {'bills':bills})
+    bills = Bill.objects.order_by('-last_action_date')[:100]
+    return render(request, 'bills/bill-listing.html', { 'bills': bills })
 
 def blog(request):
     return render(request, 'bills/blog.html')
@@ -24,10 +25,43 @@ def contact_us(request):
 
 def single(request, bill_id):
     bill = get_object_or_404(Bill, pk=bill_id)
-    return render(request, 'bills/single.html', {'bill':bill})
+    revisions = bill.billrevision_set.all()
+    return render(request, 'bills/single.html', { 
+        'bill': bill,
+        'revisions': revisions
+    })
+
+def subsection_repl(matchobj):
+    return matchobj.group(1) + '.\n('
+
+def revision(request, bill_id, rev_id):
+    print 'Fetching bill {0} revision {1}...'.format(bill_id, rev_id)
+    revision = get_object_or_404(BillRevision, pk=rev_id)
+    bill = get_object_or_404(Bill, pk=bill_id)
+
+    decoded_text = base64.b64decode(revision.doc_encoded)
+    bill_soup = BeautifulSoup(decoded_text, 'html.parser')
+    bill_soup.find('style').extract()
+    bill_text = bill_soup.get_text().split('\n')
+    bill_text = list(filter(lambda x: re.search('\w+', x), bill_text))
+    bill_text = ' '.join(bill_text)
+    # bill_text = bill_text.split(('\xa0' * 7) + 'S')
+    # bill_text = '\n\nS'.join(bill_text)
+    # bill_text = re.sub(r'(\..+[A-Z]+)(\. .?\()', subsection_repl, bill_text)
+    # bill_text = bill_text.replace(('\xa0' * 13) + '(', '\n(')
+    # bill_text = bill_text.replace(('\xa0' * 7) + '(', '\n(')
+
+    print bill_text
+
+    return render(request, 'bills/revision.html', {
+        'bill': bill,
+        'revision': revision,
+        'body': bill_text,
+        'html': decoded_text
+    })
 
 def search_by_title(request):
-    bills = Bill.objects.order_by('-date')
+    bills = Bill.objects.order_by('-last_action_date')
     try:
         bill = bills.get(title=request.POST['Title'])
     except (KeyError, Bill.DoesNotExist):
