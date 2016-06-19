095d04184b932f04134bd4573ad38e37e2059de2
John Crowe <thecrow1213@gmail.com>
2016-06-19 17:46:21 -0500

    Removing slaw stuff. This has really turned in to more of a react branch :p

--- billtracker/bills/static/bills/scripts/src/app.jsx
+++ billtracker/bills/static/bills/scripts/src/app.jsx
@@ -3,21 +3,34 @@ var ReactDOM = require('react-dom');
 var classNames = require('classnames');
 
 var AkomaNtosoHighlighter = React.createClass({
+    getInitialState: function() {
+        return {
+            value: '',
+            color: 'red'
+        };
+    },
+    componentDidMount: function(arg) { },
+    handleChange: function(event) {
+        this.setState({value: event.target.value});
+    },
     render: function() {
         var rowClass = classNames('row');
         var colClass = classNames('col-sm-6');
+
         return <div className={rowClass}>
             <div className={colClass}>
                 <h2>Insert Akoma Ntoso Document</h2>
-                <textarea></textarea>
+                <textarea value={this.state.value} onChange={this.handleChange} />
             </div>
             <div className={colClass}>
                 <h2>Display Preview</h2>
+                {/*<div dangerouslySetInnerHTML={{__html: this.state.value}} />*/}
             </div>
         </div>;
     }
 });
 
+// TODO refactor this for more dynamic rendering...
 ReactDOM.render(
   <AkomaNtosoHighlighter />,
   document.getElementById('app')
